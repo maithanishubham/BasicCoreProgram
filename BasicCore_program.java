2ed8f57eb0db9a55edb205c89cba8c18086026ed
shubham maithani <shubhammaithani1@gmail.com>
2022-07-27 11:22:03 -0700

    Heads and Tails

--- BasicCore_program.java
+++ BasicCore_program.java
@@ -4,17 +4,26 @@ public class BasicCore_program {
 
 	public static void main(String[] args) {
 		
-		Scanner reader = new Scanner(System.in);
-
-		  System.out.print("Enter a number: ");
-		  
-		  int num = reader.nextInt();
-		  if(num % 2 == 0)
-			  System.out.println(num + " is even");
-		  else
-				            System.out.println(num + " is odd");
-			
-
+		System.out.println("Welcome To Basic Core program");
+		int h_cnt = 0; int t_cnt = 0;
+		Scanner Scan = new Scanner(System.in);
+		System.out.println("Enter the no of times to flip a coin:");
+		int n = Scan.nextInt();
+		for (int i = 1; i < n; i++) {
+			double flip = Math.floor(Math.random() * 10) % 2;
+			if (flip< 0.5) {
+				h_cnt++;
+			} else {
+				t_cnt++;
 			}
-
+			
 		}
+		double headpercentage = (h_cnt* 100) /n;
+		double tailpercentage = (t_cnt * 100) /n;
+		System.out.println("The percentage of head flips:" + headpercentage);
+		System.out.println("The percentage of head flips:" + tailpercentage);
+		
+
+	}
+
+}
