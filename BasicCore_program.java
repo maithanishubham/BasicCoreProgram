fb38d119a3bd9dafa8ef98b765d6607ee8124b89
shubham maithani <shubhammaithani1@gmail.com>
2022-07-27 11:51:09 -0700

    Largest number

--- BasicCore_program.java
+++ BasicCore_program.java
@@ -4,14 +4,19 @@ public class BasicCore_program {
 	
 	public static void main(String[] args) {
 		
-		char ch = 'h';
+		int n1 = 4;   
+		 int n2 = 3;
+		int n3 = 5;
 
-        if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' )
-            System.out.println(ch + " is vowel");
-        else
-            System.out.println(ch + " is consonant");
+      if( n1 >= n2 && n1 >= n3)
+          System.out.println(n1 + " is the largest number.");
 
-	
+      else if (n2 >= n1 && n2 >= n3)
+          System.out.println(n2 + " is the largest number.");
+
+      else
+          System.out.println(n3 + " is the largest number.");
+		
 
 	}
 
