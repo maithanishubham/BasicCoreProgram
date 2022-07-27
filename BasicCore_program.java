d8583331289d007a4b14707b2d14015e6a7d88a8
shubham maithani <shubhammaithani1@gmail.com>
2022-07-27 11:18:22 -0700

    Odd or Even number

--- BasicCore_program.java
+++ BasicCore_program.java
@@ -1,9 +1,20 @@
 package Basicprogram;
-
+import java.util.Scanner;
 public class BasicCore_program {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Basic Core Program");
-	}
+		
+		Scanner reader = new Scanner(System.in);
 
-}
+		  System.out.print("Enter a number: ");
+		  
+		  int num = reader.nextInt();
+		  if(num % 2 == 0)
+			  System.out.println(num + " is even");
+		  else
+				            System.out.println(num + " is odd");
+			
+
+			}
+
+		}
